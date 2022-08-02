74706a8ec53f44c5c5f393e693aa47b6f7bebdaf
Aya Hariri <elhariri.aya@gmail.com>
2022-08-02 17:31:42 -0500

    TS-18 Basic Backend layout

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require ('express');
+const app = express();
+const AWS = require('aws-sdk');
+const port = 3000;
+const client = new AWS.DynamoDB.DocumentClient();
+
+
+app.listen(port,() => {
+    console.log("listening on port " + port);
+});
