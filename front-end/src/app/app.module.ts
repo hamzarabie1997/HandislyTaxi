1b8bdca8435e82a0ac30580a8eade8d1e944c00b
Hamza Rabie <hamza.rabie1997@gmail.com>
2022-09-09 21:36:37 +0300

    "Form created and styling done" without 'angular forms'

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -9,18 +9,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule, 
-    HeaderModule, 
+    AppRoutingModule,
+    HeaderModule,
     HomeModule,
     ContactModule,
     ServicesModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
