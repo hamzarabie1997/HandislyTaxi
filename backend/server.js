32762c67e0f38525ec552646215047eae8efc72a
Hamza Rabie <hamza.rabie1997@gmail.com>
2022-09-20 21:22:08 +0300

    'backend for emails'

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,50 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+
+const details = require("./details.json");
+
 const app = express();
-const AWS = require('aws-sdk');
-const port = 3000;
-const client = new AWS.DynamoDB.DocumentClient();
+app.use(cors({ origin: "*" }));
+app.use(bodyParser.json());
 
-
-app.listen(port, () => {
-    console.log("listening on port " + port);
-
+app.listen(3000, () => {
+  console.log("The server started on port 3000 !!!!!!");
 });
+
+app.get("/", (req, res) => {
+  res.send();
+});
+
+app.post("/sendmail", (req, res) => {
+  console.log("request came");
+  let user = req.body;
+  sendMail(user, (info) => {
+    res.send(info);
+  });
+});
+
+async function sendMail(user, callback) {
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+  });
+
+  let mailOptions = {
+    from: "TaxiApp", // sender address
+    to: user.email, // list of receivers
+    subject: "Welcome", // Subject line
+    html: `<h1>Hi ${user.name}</h1><br>
+    <h4>Thanks for joining us</h4>`,
+  };
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail(mailOptions);
+
+  callback(info);
+}
+
+// main().catch(console.error);
